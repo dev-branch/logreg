3d3c787287a0d67ed44408b6cbb237a2e5e5631a
Chyld Medford <chyld.medford@gmail.com>
2022-02-11 07:52:02 -0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
 
+
 def roc_curve(probabilities, labels):
     '''
     INPUT: numpy array, numpy array
@@ -12,8 +13,11 @@ def roc_curve(probabilities, labels):
     ROC curve.
     '''
     n = labels.shape[0]
-    fpr, tpr = np.zeros(n), np.zeros(n)
-    thr = np.linspace(0, 1, 50)
-    for t in thr:
+    fprs, tprs = np.zeros(n), np.zeros(n)
+    thrs = np.linspace(0, 1, 50)
+    for i, t in enumerate(thrs):
         predicted = probabilities >= t
         tn, fp, fn, tp = confusion_matrix(labels, predicted).flatten()
+        tprs[i] = tp / (tp + fn)
+        fprs[i] = fp / (fp + tn)
+    return fprs, tprs, thrs
